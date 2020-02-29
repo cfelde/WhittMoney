04515377cde90633f5b8ff47cbdc5c5ba335987b
Joshua Richardson <joshua@richardson.tech>
2020-02-29 23:47:01 +0000

    Mock offers listing

--- app/src/components/dashboard/tabs/tab-content/OffersTable.js
+++ app/src/components/dashboard/tabs/tab-content/OffersTable.js
@@ -2,43 +2,60 @@ import React from 'react'
 import logo from '../../../../assets/drizzle_logo.png'
 import { newContextComponents } from '@drizzle/react-components'
 import Section from '../../../Section'
-
+import bigInt from 'big-integer'
+import './OffersTable.css'
+import humanizeDuration from 'humanize-duration'
 const { AccountData, ContractData, ContractForm } = newContextComponents
+
+const offersList = [
+  {
+    id: 1,
+    collateral: bigInt('1000000000000000000000'),
+    duration: 15778800,
+    value: bigInt('6000000000000000000'),
+    contractAddress: '0xc12d1c73ee7dc3615ba4e37e4abfdbddfa38907e',
+    fulfilled: false,
+  },
+  {
+    id: 2,
+    collateral: bigInt('1000000000000000000000'),
+    duration: 7889400,
+    value: bigInt('3000000000000000000'),
+    contractAddress: '0xcc4d1cd3ee7dc3615ba4e37e4abfdbddfa38812f',
+    fulfilled: false,
+  },
+]
 
 export default props => {
   // destructure drizzle and drizzleState from props
   const { drizzle, drizzleState } = props
   return (
     <>
-      <div className="card">
-        <header className="card-header">
-          <p className="card-header-title">Component</p>
-          <a href="#" className="card-header-icon" aria-label="more options">
-            <span className="icon">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </a>
-        </header>
-        <div className="card-content">
-          <div className="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-            <br />
-            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      {offersList.map(i => {
+        return (
+          <div className="card card-offer">
+            <header className="card-header">
+              <p className="card-header-title">Offer #{i.id}</p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                {'Collateral: ' + i.collateral.divide('1000000000000000000').toString() + ' DAI'}
+                <br />
+                {'Contract: ' + i.contractAddress}
+                <br />
+                {'Duration: ' + humanizeDuration(i.duration * 1000)}
+                <br />
+                {'Value: ' + i.value.divide('1000000000000000000').toString() + ' DAI'}
+              </div>
+            </div>
+            <footer className="card-footer">
+              <a href="#" className="card-footer-item">
+                Fill Order
+              </a>
+            </footer>
           </div>
-        </div>
-        <footer className="card-footer">
-          <a href="#" className="card-footer-item">
-            Save
-          </a>
-          <a href="#" className="card-footer-item">
-            Edit
-          </a>
-          <a href="#" className="card-footer-item">
-            Delete
-          </a>
-        </footer>
-      </div>
+        )
+      })}
     </>
   )
 }
