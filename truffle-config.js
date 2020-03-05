6864ee688a04fc741ab74c71cea536bface291ea
Christian Felde <cfelde@cfelde.com>
2020-03-05 20:07:04 +0000

    Use default build location

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,9 @@
-const path = require("path");
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        develop: {
+            port: 8545
+        }
     }
-  }
 };
